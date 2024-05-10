8c7e0807ebb65e6a0b5d914ce4429967a2429d22
Djanhinho <feliperodeadriano@outlook.com>
2024-05-10 11:18:14 -0300

    Initial text typed

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("=======================================");
+        System.Console.WriteLine("   Bem-vindo(a) a minha calculadora!");
+        System.Console.WriteLine("=======================================");
+        System.Console.WriteLine();
+        System.Console.WriteLine("Qual cálculo você gostaria de realizar?");
+        System.Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Raiz Quadrada\n6 - Fechar");
     }
 }
