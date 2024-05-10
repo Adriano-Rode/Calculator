fb7235af0f62bd9c084d71fcb7d06ba93d1619a1
Djanhinho <feliperodeadriano@outlook.com>
2024-05-10 11:40:26 -0300

    Creating an auxiliary method

--- Program.cs
+++ Program.cs
@@ -4,11 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        System.Console.WriteLine("=======================================");
-        System.Console.WriteLine("   Bem-vindo(a) a minha calculadora!");
-        System.Console.WriteLine("=======================================");
-        System.Console.WriteLine();
-        System.Console.WriteLine("Qual cálculo você gostaria de realizar?");
-        System.Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Raiz Quadrada\n6 - Fechar");
+        Console.WriteLine("=======================================");
+        Console.WriteLine("   Bem-vindo(a) a minha calculadora!");
+        Console.WriteLine("=======================================");
+        Console.WriteLine();
+        Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Raiz Quadrada\n6 - Fechar");
+        Console.WriteLine();
+        Console.Write("Qual cálculo você gostaria de realizar? ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] s;
+
+        switch(n)
+        {
+            case 1: //Soma
+                Console.Write("Digite os números que deseja somar: ");
+                s = Console.ReadLine().Split(' ');
+                
+                break;
+            case 2: //Subtração\
+                Console.Write("Digite os números que deseja subtrair: ");
+                s = Console.ReadLine().Split(' ');
+
+                break;
+            case 3: //Multiplicação
+                Console.Write("Digite os números que deseja multiplicar: ");
+                s = Console.ReadLine().Split(' ');
+
+                break;
+            case 4: //Divisão
+                Console.Write("Digite os números que deseja dividir: ");
+                s = Console.ReadLine().Split(' ');
+
+                break;
+            case 5: //Raiz Quadrada
+                break;
+            default:
+                return;
+        }
+    }
+
+    static double[] ConvertToDouble(string[] s)
+    {
+        double[] v = new double[s.Length];
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            v[i] = double.Parse(s[i]);
+        }
+
+        return v;
     }
 }
